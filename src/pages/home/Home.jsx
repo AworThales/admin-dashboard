18b8aec96f729c6258a412ee23ef043362de7a2a
AworThales <thals4jesus@gmail.com>
2023-06-23 04:59:10 -0700

    Added Navbar component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,15 @@
 import Sidebar from '../../components/sidebar/Sidebar'
+import Navbar from '../../components/navbar/Navbar'
 import './home.scss'
 
 const Home = () => {
   return (
     <div className='home'>
       <Sidebar />
-      <div className="homeContainer">Conatiner</div>
+      <div className="homeContainer">
+        <Navbar />
+        Home Conatiner
+      </div>
     </div>
   )
 }
