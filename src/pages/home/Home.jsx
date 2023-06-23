22bf6539ca9ee27cc889a8c6eb33544cce53f96f
AworThales <thals4jesus@gmail.com>
2023-06-23 09:29:33 -0700

    updated charts components

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,8 @@ import Sidebar from '../../components/sidebar/Sidebar'
 import Navbar from '../../components/navbar/Navbar'
 import './home.scss'
 import Widget from '../../components/widget/Widget'
+import Featured from '../../components/featured/Featured'
+import Chart from '../../components/chart/Chart'
 
 const Home = () => {
   return (
@@ -9,11 +11,17 @@ const Home = () => {
       <Sidebar />
       <div className="homeContainer">
         <Navbar />
+        {/* widget */}
         <div className="widgets">
           <Widget type="user" />
           <Widget type="order" />
           <Widget type="earning" />
           <Widget type="balance" />
+        </div>
+        {/* chart container*/}
+        <div className="charts">
+          <Featured />
+          <Chart />
         </div>
       </div>
     </div>
