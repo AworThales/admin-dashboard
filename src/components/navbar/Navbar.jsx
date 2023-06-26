2bfba39adc68dc58c1c0a4b565e7a1f395a80e00
AworThales <thals4jesus@gmail.com>
2023-06-26 06:03:44 -0700

    Added icons

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,11 @@
 import './navbar.scss';
-// import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
-// import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
-// import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
-// import FullscreenExitOutlinedIcon from '@mui/icons-material/FullscreenExitOutlined';
-// import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
-// import ChatBubbleOutlinedIcon from '@mui/icons-material/ChatBubbleOutlined';
-// import ListOutlinedIcon from '@mui/icons-material/ListOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import FullscreenExitOutlinedIcon from '@mui/icons-material/FullscreenExitOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import ChatBubbleOutlinedIcon from '@mui/icons-material/ChatBubbleOutlined';
+import ListOutlinedIcon from '@mui/icons-material/ListOutlined';
 
 const Navbar = () => {
   return (
@@ -13,29 +13,29 @@ const Navbar = () => {
       <div className="wrapper">
         <div className="search">
           <input type="text" placeholder='Search...' />
-          {/* <SearchOutlinedIcon /> */}
+          <SearchOutlinedIcon />
         </div>
         <div className="items">
           <div className="item">
-            {/* <LanguageOutlinedIcon className="icon" /> */}
+            <LanguageOutlinedIcon className="icon" />
             English
           </div>
           <div className="item">
-            {/* <DarkModeOutlinedIcon className="icon" /> */}
+            <DarkModeOutlinedIcon className="icon" />
           </div>
           <div className="item">
-            {/* <FullscreenExitOutlinedIcon className="icon" /> */}
+            <FullscreenExitOutlinedIcon className="icon" />
           </div>
           <div className="item">
-            {/* <NotificationsOutlinedIcon className="icon" /> */}
+            <NotificationsOutlinedIcon className="icon" />
             <div className="counter">1</div>
           </div>
           <div className="item">
-            {/* <ChatBubbleOutlinedIcon className="icon" /> */}
+            <ChatBubbleOutlinedIcon className="icon" />
             <div className="counter">2</div>
           </div>
           <div className="item">
-            {/* < ListOutlinedIcon className="icon" /> */}
+            < ListOutlinedIcon className="icon" />
           </div>
           <div className="item">
             <img
